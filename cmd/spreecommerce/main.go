5f579d4adf9d27a838300fa15ef902d538e90a14
Willy <128942050+Willysc10@users.noreply.github.com>
2025-10-01 21:46:13 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/spreecommerce/main.go
@@ -0,0 +1,23 @@
+// cmd/spreecommerce/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "spreecommerce/internal/spreecommerce"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := spreecommerce.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
